8df3eb0042b00ce5d00d8d543a4dcd750ac745df
capurrex <capurrex@gmail.com>
2023-02-13 16:31:37 -0300

    subo pruyecto luar

--- js/main.js
+++ js/main.js
@@ -1,151 +1,135 @@
-//funciones
-
-function convertir(conversor, unidadIngresada, unidadDeseada) {
-  let valorAConvertir;
-  do {
-    valorAConvertir = parseInt(
-      prompt(
-        `Ingrese la cantidad de ${unidadIngresada} que desea convertir a ${unidadDeseada}, solo puede ingresar numeros`
-      )
-    );
-  } while (isNaN(valorAConvertir));
-  let valorConvertido = conversor * valorAConvertir;
-  alert(
-    `${valorAConvertir} ${unidadIngresada} son ${valorConvertido} ${unidadDeseada}`
-  );
-}
-
+//constructora
 class velas {
-  constructor(nombre, precio, descripcion) {
+  constructor(id,nombre, precio, imagen, imgAlt, descripcion) {
+    this.id = id,
     this.nombre = nombre;
     this.precio = precio;
-    this.descripcion = descripcion;
+    this.imagen = imagen;
+    this.imgAlt = imgAlt;
+    this.descripcion=descripcion;
   }
 }
 
-//declaracion objetos y arrays
+//capturas nodos 
+let verCatalogo = document.getElementById("verCatalogo")
+let match = document.getElementById("match")
+let chartModal = document.getElementById("chartModal")
 
-const vela1 = new velas("Woolen ball", 150, "Vela con forma de ovillo de lana");
-const vela2 = new velas("Bubble", 350, "Vela con forma de 9 bolas unidas ");
-const vela3 = new velas("Mini bubble", 100, "Vela con forma de 4 bolas unidas");
-const vela4 = new velas("Onion", 180, "Vela con forma de gota  espiralada");
-const vela5 = new velas("Luna llena", 550, "Vela con forma de luna completa");
-const vela6 = new velas("Luna nueva", 450, "Vela con forma de  ");
-const vela7 = new velas(
-  "Cuarto creciente",
-  350,
-  "Vela con forma de forma de media luna con panza hacia abajo"
-);
-const vela8 = new velas(
-  "Cuarto menguante",
-  300,
-  "Vela con forma de media luna con panza hacia arriba"
-);
-const vela9 = new velas("Yue", 200, "Vela en frasco con forma de luna ");
-const vela10 = new velas("Lúa", 200, "Vela en frasco regular ");
-const vela11 = new velas(
-  "Bombones de cera X 5",
-  100,
-  "Velitas con formas variades para colocar en el hornito"
-);
-const vela12 = new velas(
-  "Hornito + bombones",
-  350,
-  " Hornito con velitas, varias formas "
-);
+//funciones
 
-/*for( let i=0 ; i<=12; i++) {
-  const grupoVelas = [vela(i)]
-}*/
-
-const grupoVelas = [
-  vela1,
-  vela2,
-  vela3,
-  vela4,
-  vela5,
-  vela6,
-  vela7,
-  vela8,
-  vela9,
-  vela10,
-  vela11,
-  vela12,
-];
-
-const aromas = [
-  "Limon",
-  "Lavanda",
-  "Mango",
-  "Vainilla",
-  "Floral",
-  "Jazmin",
-  "Naranja",
-  "Canela",
-];
-
-//menu de conversion
-
-/*let opcion = parseInt(
-  prompt(`Ingrese el numero de la opcion deseada de las unidades que desea convertir:
-      1. De gramos a onzas
-      2. De onzas a gramos
-      3. De millas a kilometros
-      4. De kilometros a millas
-      0. Salir `)
-);
-
-switch (opcion) {
-  case 1:
-    convertir(0.035, "gramos", "onzas");
-    break;
-  case 2:
-    convertir(28.3, "onzas", "gramos");
-    break;
-  case 3:
-    convertir(1.61, "millas", "kilometros");
-    break;
-  case 4:
-    convertir(0.62, "kilometros", "millas");
-    break;
-  case 0:
-    alert(`gracias por preferirnos`);
-    break;
-  default:
-    alert(`Usted no a ingresado una opcion correcta`);
-    break;
-}*/
-
-grupoVelas.sort((a, b) => b.precio - a.precio);
-
-for (let pagoDescuento of grupoVelas) {
-  if (pagoDescuento.precio > 240) {
-    console.log(
-      `El precio de ${pagoDescuento.nombre} seria $ ${pagoDescuento.precio}`
-    );
-    pagoDescuento.precio = pagoDescuento.precio * 0.8;
-    console.log(
-      `Pero con tu descuento te queda solo $ ${pagoDescuento.precio} !!`
-    );
-  } else {
-    console.log(
-      `El precio de ${pagoDescuento.nombre} seria $ ${pagoDescuento.precio}`
-    );
-  }
-}
-
-{let input = prompt(
-  `Puedes ver los precios en la consola, pero puedes escribir aqui el nombre de la vela para recibir su descripcion`
-);
-
-let buscado = grupoVelas.find(
-  (busqueda) => busqueda.nombre.toLowerCase() == input.toLowerCase()
-)
-
-if (buscado == undefined) {
-  alert (`Este articulo es inexistente`)
-}
- else {
-  alert (`${buscado.descripcion}`);
+const Catalogo = async ()=> {
+  const response = await fetch("velas.json")
+  const data =await response.json()
+  for(let candle of data){
+    let newCandle = new velas (candle.id,candle.nombre,candle.precio,candle.imagen,candle.imagenAlt,candle.descripcion )
+     grupoVelas.push(newCandle)
 }}
 
+function mostrarCatalogo(arrVela){
+  Catalogo()
+  verCatalogo.innerHTML = "" 
+  console.log(arrVela)
+  console.log(arrVela.length)
+  for(let vela of arrVela){
+      let nuevaVela = document.createElement("div")
+      nuevaVela.classList.add ("col-xl-2","col-md-3", "col-6","row")
+      nuevaVela.innerHTML = ` <div id="${vela.id}" class="card row">
+        <img src="${vela.imagen}" class="card-img-top" alt="${vela.imgAlt}" />
+        <div class="card-body">
+          <h5 class="card-title">${vela.nombre}</h5>
+          <p class="card-text">
+            ${vela.descripcion}. Precio: $ ${vela.precio}
+          </p>
+          <a  id ="carritov${vela.id}" class="btn">Agregar al carrito</a>
+          <div class="row">
+            <ul class="mitad">
+              <select
+                id="selectAroma"
+                class="form-select form-select-sm"
+                aria-label=".form-select-sm example">
+                  <option selected>Elije el aroma</option>
+                  <option value="li">Limón</option>
+                  <option value="la">Lavanda</option>
+                  <option value="ma">Mango</option>
+                  <option value="va">Vainilla</option>
+                  <option value="fl">Floral</option>
+                  <option value="ja">Jazmin</option>
+                  <option value="na">Naranja</option>
+                  <option value="ca">Canela</option>
+              </select>
+            </ul>
+            <ul class="mitad">
+              <select
+                id="selectColor"
+                class="form-select form-select-sm"
+                aria-label=".form-select-sm example">
+                  <option selected>Elije el color</option>
+                  <option value="roc">Rosa</option>
+                  <option value="vec">Verde</option>
+                  <option value="azc">Azul</option>
+                  <option value="amc">Amarillo</option>
+                  <option value="grc">Gris</option>
+                  <option value="bec">Beige</option>
+                  <option value="nac">Naranja</option>
+                  <option value="blc">Blanco</option>
+                  <option value="vic">Violeta</option>
+              </select>
+            </ul>
+          </div>
+        </div>
+    </div>`
+
+     verCatalogo.appendChild(nuevaVela)
+
+     let botonCarrito = document.getElementById(`carritov${vela.id}`)
+         
+        botonCarrito.addEventListener("click", ()=>{addChart(vela)})
+
+
+  }}
+
+
+function addChart (vela) {
+  chartItems.push(vela)
+  localStorage.setItem("chart", JSON.stringify(chartItems))
+}
+
+function showChart(array){
+
+  for(let item of array){
+      let newItem = document.createElement("div")
+
+      newItem.innerHTML = `<div class="card mb-3" style="max-width: 540px;">
+      <div class="row g-0">
+        <div class="col-md-4">
+          <img src="${item.imagen}" class="img-fluid rounded-start imgSizeAdjust" alt="${item.imgAlt}">
+        </div>
+        <div class="col-md-8 text-center hstack">
+          <div class="card-body align-evenly">
+            <h5 class="card-title">${item.nombre}</h5>
+            <p class="card-text">${item.descripcion} <br> Color:$colo{}  Aroma:$aroma{}  Precio: $ ${item.precio  } </p>
+            <a id="removeCh${item.id}" class="btn btn-danger">Eliminar</a>
+          </div>
+        </div>
+      </div>
+    </div>`
+     chartModal.appendChild(newItem)
+    }}
+
+
+
+//instanciacion
+
+let grupoVelas =[]
+
+let chartItems = JSON.parse(localStorage.getItem("chart")) || []
+
+// eventos
+
+
+
+mostrarCatalogo(grupoVelas)
+
+showChart(chartItems)
+
+//console.log(grupoVelas)
